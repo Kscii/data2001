b64ec6da716bbd691060343c2bcc8e10ff41f3d7
Daniel Kaiqi Bi <dabi0142@uni.sydney.edu.au>
2026-05-10 23:16:41 +1000

    Update convert_numeric_columns.py

--- src/task1_cleaning/convert_numeric_columns.py
+++ src/task1_cleaning/convert_numeric_columns.py
@@ -2,5 +2,14 @@ import pandas as pd
 
 
 def convert_numeric_columns(df: pd.DataFrame) -> pd.DataFrame:
-    # 需要返回一个 pandas DataFrame 对象
-    raise NotImplementedError("convert_numeric_columns is not implemented yet")
+    df = df.copy()
+
+    #找出所有年份列
+    year_columns = [col for col in df.columns if col.isdigit()]
+
+    #将年份列转换为数值
+    #无法转换的内容会自动变成NaN
+    for col in year_columns:
+        df[col] = pd.to_numeric(df[col], errors="coerce")
+
+    return df
