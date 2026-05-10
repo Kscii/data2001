1968fb0976612ff2c216168331b61b961ac3b0bb
Daniel Kaiqi Bi <dabi0142@uni.sydney.edu.au>
2026-05-10 23:15:38 +1000

    Update clean_column_names.py

--- src/task1_cleaning/clean_column_names.py
+++ src/task1_cleaning/clean_column_names.py
@@ -4,12 +4,13 @@ import pandas as pd
 def clean_column_names(df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
 
+    # 清理列名方便后续Python处理
     df.columns = (
         df.columns
-        .str.strip()
-        .str.lower()
-        .str.replace(" ", "_")
-        .str.replace(r"[^\w]", "", regex=True)
+        .str.strip()                     #去除前后空格
+        .str.lower()                    #转成小写
+        .str.replace(" ", "_")          #空格替换为下划线
+        .str.replace(r"[^\w]", "", regex=True)  #删除特殊符号
     )
 
     return df
